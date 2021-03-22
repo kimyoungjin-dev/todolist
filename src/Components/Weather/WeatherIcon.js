032f97239edbb07f26d69b7e1ede3d3f6e8c0d56
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-22 19:33:06 +0900

    Create a component that changes to an icon for each weather name.

---
+++ src/Components/Weather/WeatherIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { AiOutlineAntCloud } from "react-icons/ai";
+import { IoThunderstorm } from "react-icons/io5";
+import { RiDrizzleFill } from "react-icons/ri";
+import { BiCloudRain } from "react-icons/bi";
+import { FaSnowflake, FaRegSun, FaSmog } from "react-icons/fa";
+import { RiHazeLine } from "react-icons/ri";
+import { TiWeatherWindy } from "react-icons/ti";
+
+export const weathers = {
+  Clouds: <AiOutlineAntCloud />,
+  Drizzle: <RiDrizzleFill />,
+  Thunderstorm: <IoThunderstorm />,
+  Rain: <BiCloudRain />,
+  Snow: <FaSnowflake />,
+  Clear: <FaRegSun />,
+  Haze: <RiHazeLine />,
+  Mist: <FaSmog />,
+  Dust: <TiWeatherWindy />,
+};
